0537c464bb1cb8ed929ab1652155f5a8e8989341
Daniel Speisky <danispy2000@gmail.com>
2020-03-19 19:55:28 +0200

    led is working and calculated baud-rate

--- try1/main.c
+++ try1/main.c
@@ -1,45 +1,58 @@
 #include "stdint.h"
 
 #define RCC 0x40021000 //resert and clock control
-#define RCC_APB1RSTR_OFFSET 0x10 //need to turn on bit 18 to reset usart3's clocks
-#define RCC_APB1ENC_OFFSET 0x1c //need to turn on bit 18 to enable usart3's clock
-#define RCC_VALUES 0x20000
+#define RCC_APB2ENR_OFFSET 0x18 //need to turn on bit 14 to reset usart1's clock
 
 #define GPIOC_BASE 0x40011000
 #define GPIOX_CRH_OFFSET 0x04
-#define CRH_VALUE 0x100000
-#define GPIOX_BSRR_OFFSET 0x10 //set or reset the led
-#define SET_LED 0x2000 //turn on
-#define RESET_LED 0x20000000 //
+#define GPIOX_ODR_OFFSET 0x0c //output register
 
-#define USART3 0x40004800 //bit 5 in the status register "read data register not empty"
+#define USART1 0x40003800 //bit 5 in the status register "read data register not empty"
 #define USART_DR_OFFSET 0x04 //insert data to transmit or receive data
 #define USART_BRR_OFFSET 0x08 //insert frequency of the usart
-#define BRR_VALUE 0 //TODO: ask or calculate //
+#define USART_CR2_OFFSET 0x10 //insert number of stop bits
 #define USART_CR1_OFFSET 0x0c //turn on bits 2,3 and 13 to enable usart,transmitter and receiver
+
+#define AFIO_BASE 0x40010000
+#define AFIO_MAPR_OFFSET 0x04
+
+#define SET_LED 0 //turn on
+#define RESET_LED 0x2000 //#define CRH_VALUE 0x100000
+#define RCC_VALUES 0x20000
+#define APB2_VALUES 0x4010
 #define CR1_VALUES 0x200C
+#define CRH_VALUE 0x100000
+#define AFIO_MAPR_VALUE 2
+#define USARTDIV_VALUE 0x1D4C //468.75
+
+char send_zero = '0';
+char send_one = '1';
 
 void configRegisters()
 {
-	uint32_t* address = (uint32_t*)(RCC+RCC_APB1RSTR_OFFSET);
-	*address = RCC_VALUES;
+	uint32_t* address = (uint32_t*)(RCC+RCC_APB2ENR_OFFSET);
+	*address = APB2_VALUES;
 	
-	address = (uint32_t*)(RCC+RCC_APB1ENC_OFFSET);
-	*address = RCC_VALUES;
+	address = (uint32_t*)(AFIO_BASE+AFIO_MAPR_OFFSET);
+	*address = AFIO_MAPR_VALUE;
 		
 	address = (uint32_t*)(GPIOC_BASE+GPIOX_CRH_OFFSET);
 	*address = CRH_VALUE;
 	
-	address = (uint32_t*)(USART3+USART_BRR_OFFSET);
-	*address = BRR_VALUE;
+	address = (uint32_t*)(USART1+USART_BRR_OFFSET);
+	*address = USARTDIV_VALUE;
 	
-	address = (uint32_t*)(USART3+USART_CR1_OFFSET);
+	address = (uint32_t*)(USART1+USART_CR2_OFFSET);
+	*address = 0;
+	
+	address = (uint32_t*)(USART1+USART_CR1_OFFSET);
 	*address = CR1_VALUES;
 }
 
+
 void led(int state)
 {
-	uint32_t* address = (uint32_t*)(GPIOC_BASE+GPIOX_BSRR_OFFSET);
+	uint32_t* address = (uint32_t*)(GPIOC_BASE+GPIOX_ODR_OFFSET);
 	if(state == 1)
 		*address = SET_LED;
 	else
@@ -49,5 +62,11 @@ void led(int state)
 int main()
 {
 	configRegisters();
+	led(0);
+	while(1)
+	{
+		
+	}
 	return 0;
 }
+
